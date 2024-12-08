742504c369f71323b1cd47a87912fcf44ec0dd94
Sabria <sislam@DESKTOP-VRDI2G4>
2024-12-07 21:30:16 -0500

    changes

---
+++ app/components/Header.js
@@ -0,0 +1,42 @@
+"use client";
+import Link from "next/link";
+import { useAuth } from "../context/AuthUserContext";
+
+export default function Header() {
+    const {signOut, authUser} = useAuth();
+
+    return (
+    <header>
+      <div>
+        <h1>Users & Auth</h1>
+      </div>
+      <nav>
+        <ul>
+          {authUser && (
+            <>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <a onClick={signOut}>Log out</a>
+              </li>
+            </>
+          )}
+
+          {!authUser && (
+            <>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <Link href="/create">Create user</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+//header from exercise 6
